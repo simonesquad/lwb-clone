b72072431cffbf2caa6b5e03ac6d64e6c67519a8
Simone Ballard <simone.a.ballard@gmail.com>
2022-02-14 12:43:34 -0700

    set up app wrapper and colors

--- lwb-music/src/App.js
+++ lwb-music/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { extendTheme, ChakraProvider } from '@chakra-ui/react';
 
+// color directly from Chakra, mimicked from the original design
+const colors = {
+  brand: {
+    gray400: '#A0AEC0',
+    red400: '#F56565',
+    orange300: '#F6AD55',
+    green600: '#2F855A',
+    teal500: '#319795',
+    yellow500: '#D69E2E'
+  },
+}
 //components
 import Contact from './pages/Contact';
 import Resources from './pages/Resources';
@@ -12,6 +24,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
+    <ChakraProvider>
     <Router>
     <div className="App">
       <Switch>
@@ -24,6 +37,7 @@ function App() {
       </Switch>
     </div>
     </Router>
+    </ChakraProvider>
   );
 }
 
