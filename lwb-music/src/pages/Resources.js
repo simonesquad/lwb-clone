21ac8e23a9cccf21760784d10b7aff8e848c2102
Simone Ballard <simone.a.ballard@gmail.com>
2022-02-21 17:00:21 -0700

    links transferred

--- lwb-music/src/pages/Resources.js
+++ lwb-music/src/pages/Resources.js
@@ -1,7 +1,5 @@
 import React from 'react'
-// import Menu from '../components/MainMenu'
-import LWBlinks from '../components/LWBlinks';
-import NAlinks from '../components/NAlinks';
+
 //chakra ui styles for this section
 import {
     Tabs,
@@ -28,13 +26,38 @@ function Resources() {
 
         <TabPanels>
             <TabPanel>
-            <p>one!</p>
+            <a rel="noreferrer" href="https://en.wikipedia.org/wiki/Louis_W._Ballard" target="_blank">Louis W. Ballard | [Wikipedia]</a>
+            <br></br>
+            <a rel="noreferrer" href="https://www.okhistory.org/publications/enc/entry.php?entry=BA009" target="_blank">Ballard, Wayne Louis | [Oklahoma Historical Society]</a>
+            <br></br>
+            <a rel="noreferrer" href="https://www.britannica.com/biography/Louis-Ballard" target="_blank">Louis Ballard | [Britannica]</a>
             </TabPanel>
             <TabPanel>
-            <p>two!</p>
+            <a rel="noreferrer" href="https://kaleidoscopemusart.com/louis-wayne-ballard/#easy-footnote-bottom-1-3489" target="_blank">Louis Wayne Ballard [by Maria Sumareva and Andrew Rosenblum]</a>
+            <br></br>
+            <a rel="noreferrer" href="https://italianacademy.columbia.edu/event/american-voices-selected-piano-works-by-black-and-native-american-composers" target="_blank">American Voices: Selected Piano Works by Black and Native American Composers</a>
             </TabPanel>
             <TabPanel>
-            <p>three!</p>
+            <a rel="noreferrer" href="https://www.okhistory.org/index.php" target="_blank">Oklahoma Historical Society</a>
+            <br></br>
+            <a rel="noreferrer" href="https://indiancountrytoday.com/archive/12-images-seneca-indian-boarding-school" target="_blank">12 Images From Seneca Indian Boarding School [Indian Country Today]</a>
+            </TabPanel>
+            <TabPanel>
+            <a rel="noreferrer" href="https://www.quapawtribe.com/" target="_blank">Quapaw Nation [Official Website]</a>
+            <br></br>
+            <a rel="noreferrer" href="https://www.cherokee.org/" target="_blank">Cherokee Nation [Official Website]</a>
+            <br></br>
+            <a rel="noreferrer" href="https://www.osagenation-nsn.gov/" target="_blank">Osage Nation [Official Website]</a>
+            </TabPanel>
+            <TabPanel>
+            <a rel="noreferrer" href="https://americanindian.si.edu/" target="_blank">National Museum of the American Indian [Smithsonian]</a>
+            <br></br>
+            <a rel="noreferrer" href="https://famok.org/" target="_blank">First Americans Museum [Oklahoma]</a>
+            <br></br>
+            <a rel="noreferrer" href="https://composersforum.org/resources/organizations-supporting-composers-new-music/" target="_blank">American Composers Forum [Supporting Organizations]</a>
+            </TabPanel>
+            <TabPanel>
+            <a rel="noreferrer" href="https://ufdc.ufl.edu/UFE0046553/00001" target="_blank">Louis W. Ballard: Composer and Music Educator [by Karl Erik Ettinger]</a>
             </TabPanel>
         </TabPanels>
         </Tabs>
