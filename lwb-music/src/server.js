df639a9b5f4b7c41af8584f6060883f062526f81
Simone Ballard <simone.a.ballard@gmail.com>
2022-05-02 08:46:37 -0500

    getting closer but post failing

--- lwb-music/src/server.js
+++ lwb-music/src/server.js
@@ -2,19 +2,29 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-// require('dotenv').config();
+const dotenv = require('dotenv');
 
+// Set path to .env file:
+dotenv.config({ path: './.env' });
+
+// Initializing the app here:
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/", router);
+// Assigning the route:
+app.use("/", (router, req, res, next) => {
+  const email = process.env.USER_GMAIL;
+  const password = process.env.PW_GMAIL;
+  res.status(200).json({ email, password });
+});
+//Starting the server:
 app.listen(5000, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: '',
-    pass: '',
+    user: email,
+    pass: password,
   },
 });
 
