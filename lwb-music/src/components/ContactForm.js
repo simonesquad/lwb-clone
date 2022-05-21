23d5859e9463933496cb173f6e481d8bd4febcc1
Simone Ballard <simone.a.ballard@gmail.com>
2022-05-21 17:39:22 -0500

    partial dark mode added with hooks

--- lwb-music/src/components/ContactForm.js
+++ lwb-music/src/components/ContactForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import '../styles/ContactForm.css';
 import { Box } from '@chakra-ui/react';
 import { Buttons } from '../components'
