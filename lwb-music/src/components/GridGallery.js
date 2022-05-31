3f0c2c818e36139b0156576bf4532e8b6d9c1207
Simone Ballard <simone.a.ballard@gmail.com>
2022-05-31 14:38:23 -0500

    captions added

--- lwb-music/src/components/GridGallery.js
+++ lwb-music/src/components/GridGallery.js
@@ -15,10 +15,14 @@ import g12 from '../assets/gallery/g_12.png';
 
 
 import {
+    Box,
     Grid,
     GridItem,
+    Heading,
     Image,
     Stack,
+    StackDivider,
+    Spacer,
     Text,
     HStack,
     VStack
@@ -38,11 +42,21 @@ const breakpoints = createBreakpoints({
           <Stack
             align='center'
           >
+              <VStack 
+                spacing={1}
+                align='center'
+            >
               <HStack
-                w='100%'
-                h='100vh'
+                w='95%'
+                h='120vh'
               >
                 <Image src={g1} alt='koshare-ballet' />
+                <Spacer />
+                <Box
+                    w='40vw'
+                >
+                <Heading as='h4' size='lg'>The choreographer Donald Saddler with ballerina Marjorie Tallchief, dancers, and Louis W. Ballard seated in the background for rehearsal of the ballet Koshare.</Heading>
+                </Box>
               </HStack>
 
               <VStack
@@ -50,17 +64,33 @@ const breakpoints = createBreakpoints({
                 h={{
                     sm: '34vh',
                     md: '60vh',
-                    lg: '100vh'
+                    lg: '120vh'
                 }}
               >
-            <Grid templateColumns='repeat(2, 1fr)' gap={10}>
-                <GridItem w='90' h='20'>
-                    <Image src={g2} alt='lwb-flute' />
-                </GridItem>
-                <GridItem w='90' h='20'>
-                    <Image src={g4} alt='koshare-piano' />
-                </GridItem>
-            </Grid>
+            <Box
+                h='95vh'
+
+            >
+                <Grid templateColumns='repeat(2, 1fr)' gap={10}>
+                    <GridItem w='90' h='20'>
+                        <Image src={g2} alt='lwb-flute' />
+                    </GridItem>
+                    <GridItem w='90' h='20'>
+                        <Image src={g4} alt='koshare-piano' />
+                    </GridItem>
+                </Grid>
+            </Box>
+            <Box
+                h='15vh'
+                w='70vw'
+                marginTop='6vh'
+            >
+                <Heading as='h4' size='md'>
+                From Left to Right:
+                Ballard holding a Sioux flute from his personal collection.
+                Donald Sadler and Louis W. Ballard along with George Skibine director of the Harkness Ballet in rehearsal for the 1965 performance of Koshare.
+                </Heading>
+            </Box>
               </VStack>
 
 
@@ -69,9 +99,12 @@ const breakpoints = createBreakpoints({
                 h={{
                     sm: '34vh',
                     md: '60vh',
-                    lg: '100vh'
+                    lg: '80vh'
                 }}
               >
+            <Box
+                h='65vh'
+            >
             <Grid templateColumns='repeat(2, 1fr)' gap={5}>
                 <GridItem w='50' h='20'>
                 <Image src={g5} alt='me' />
@@ -80,13 +113,30 @@ const breakpoints = createBreakpoints({
                     <Image src={g12} alt='snow' />
                 </GridItem> 
             </Grid>
+            </Box>
+            
+            <Box
+                h='15vh'
+                w='70vw'
+                marginTop='6vh'
+            >
+            <Heading as='h4' size='md'>
+            The world premiere of the Portrait of Will Rogers in 1972. Pictured on the left are Dr. Wesley Forbis and Will Rogers Jr. in the center alongside Ballard.
+            </Heading>
+            </Box>
               </VStack>
 
               <HStack
-                w='100%'
+                w='95%'
                 h='100vh'
-                align='center'
               >
+                <Box
+                    w='40vw'
+                >
+                <Heading as='h4' size='md'>
+                The composer in a promotional shot that he often used. Probably taken in Oklahoma, USA.
+                </Heading>
+                </Box>
                 <Image src={g8} alt='lwb-flute2' />
               </HStack>
 
@@ -98,7 +148,18 @@ const breakpoints = createBreakpoints({
                     lg: '100vh'
                 }}
               >
-
+            <Box
+                h='15vh'
+                w='70vw'
+                marginTop='6vh'
+                >
+                <Heading as='h4' size='md'>
+                On the Right: Sammy Davis Jr. and Louis W. Ballard attending the Tulsa Philharmonic Silver Anniversary in 1973. Sammy Davis Jr. performed and two Ballard pieces were performed.
+                </Heading>
+            </Box>
+            <Box
+                h='55vh'
+            >
             <Grid templateColumns='repeat(2, 1fr)' gap={7}>
                 <GridItem w='50' h='20'>
                     <Image src={g9} alt='tree' />
@@ -107,8 +168,10 @@ const breakpoints = createBreakpoints({
                     <Image src={g11} alt='water' />
                 </GridItem>
             </Grid>
-              </VStack>
-          </Stack>
+            </Box>
+            </VStack>
+        </VStack>
+    </Stack>
       )
   }
 
