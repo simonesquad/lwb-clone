fa58cd3ad44fc365f786f0bad9a8209aab9fe07c
Simone Ballard <simone.a.ballard@gmail.com>
2022-06-01 13:02:00 -0500

    greeting breakpoints updated

--- lwb-music/src/components/Greeting.js
+++ lwb-music/src/components/Greeting.js
@@ -5,17 +5,34 @@ import {
     Heading,
     Text
 } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const breakpoints = createBreakpoints({
+    sm: '30em',
+    md: '48em',
+    lg: '62em',
+    xl: '80em',
+    '2xl': '96em',
+  })
 
 const Greeting = () => {
   return (
     <Box w='100vw' h='50vh'>
         <Box 
-          w='50vw' 
+          w={{
+            sm: '75vw',
+            md: '55vw',
+            lg:'50vw' 
+          }}
           h='50vh'
           marginTop='15%'
           marginLeft='10%'
         >
-          <Box h='9vh'>
+          <Box h={{
+            sm: '13vh',
+            md: '14vh',
+            lg: '9vh'
+          }}>
             <Heading>Thank You for Reaching Out!</Heading>
           </Box>
             <Text>We appreciate the continued interest in Louis W. Ballard music and history. We will get back to your inquiry as soon as we can.</Text>
