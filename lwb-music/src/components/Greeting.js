4ed47a985622bb76ad1dbd957ae52e4672de0db0
Simone Ballard <simone.a.ballard@gmail.com>
2024-11-10 14:17:37 -0600

    updating greeting page

--- lwb-music/src/components/Greeting.js
+++ lwb-music/src/components/Greeting.js
@@ -20,52 +20,38 @@ const breakpoints = createBreakpoints({
 
 const Greeting = () => {
   return (
-    <Box w='100vw' h='50vh'>
-        <Box 
-          w={{
-            sm: '75vw',
-            md: '55vw',
-            lg:'50vw' 
-          }}
-          h='50vh'
-          marginTop='15%'
-          marginLeft='10%'
-        >
-          <Box h={{
-            sm: '13vh',
-            md: '14vh',
-            lg: '9vh'
-          }}>
-            <Heading>Thank You for Reaching Out Over the LWB Music Project!</Heading>
-          </Box>
-          <VStack
+    <Box w='100vw'>
+        <VStack
             align='center'
             w='90vw'
           >
+          <Box h={{
+            sm: '17vh',
+            md: '19vh',
+            lg: '22vh'
+          }}>
+            <Heading>Thank You for Reaching Out Over the LWB Music Project!</Heading>
+          </Box>
+          
           <Box>
             <Text>
               We appreciate the continued interest in the Louis W. Ballard music, life, and history. The team will get back to your inquiry as soon as they can. If you requested a certain music piece, please also check the music section of the website with an updated list of scores readily available. Other pieces might be found with publishers nationwide. Enjoy the #music
             </Text>
             </Box>
-            <Box h={{
-              sm: '20vh',
-              md: '22vh',
-              lg: '23vh'
-            }}>
+            <Box>
             <Image
               src={FiveMoons} 
               w='100%'
-              // height={{
-              //     sm: '45%',
-              //     md: '35%',
-              //     lg: '65%',
-              //     xl: '85%',
-              // }}
+              h={{
+                  sm: '45%',
+                  md: '55%',
+                  lg: '65%',
+                  xl: '85%',
+              }}
             />
           </Box>
         </VStack>
-        </Box>
-    </Box>
+      </Box>
   )
 }
 
